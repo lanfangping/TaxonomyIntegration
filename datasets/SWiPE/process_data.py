c6c1eeec3a9440b5bbe3cc9c3954c5356312fec5
Fangping Lan <34638594+lanfangping@users.noreply.github.com>
2024-08-18 00:03:13 -0400

    Wiki-pure(SWiPE) data and samping

--- datasets/SWiPE/process_data.py
+++ datasets/SWiPE/process_data.py
@@ -2,10 +2,9 @@ import json
 import utils_diff
 from tqdm import tqdm
 
-processed_data = []
-with open("data/swipe_train.json", "r") as f: # See: data/swipe_val.json, data/swipe_test_id.json, data/swipe_test_ood.json for the validation, in-domain test, and out-of-domain test sets
-    swipe_train = json.load(f)
-    for i in tqdm(range(len(swipe_train))):
+def process(data):
+    processed_data = []
+    for i in tqdm(range(len(data))):
         sample = swipe_train[i]
         edits = utils_diff.get_edit_operations(sample["r_content"], sample["s_content"], split_replace=True, split_sentences=True)
         # for edit in edits:
@@ -63,6 +62,19 @@ with open("data/swipe_train.json", "r") as f: # See: data/swipe_val.json, data/s
                 "before_token_range": before_token_range,
                 "after_token_range": after_token_range
             })
+    return processed_data
+
+processed_data = []
+with open("data/swipe_train.json", "r") as f: # See: data/swipe_val.json, data/swipe_test_id.json, data/swipe_test_ood.json for the validation, in-domain test, and out-of-domain test sets
+    swipe_train = json.load(f)
+    data = process(swipe_train)
+    processed_data.extend(data)
+
+with open("data/swipe_val.json", "r") as f:
+    swipe_val = json.load(f)
+    data = process(swipe_val)
+    processed_data.extend(data)
+    
 print("Number of edits:", len(processed_data))
 with open('processed_swipe_data.json', 'w') as json_file:
     json.dump(processed_data, json_file, indent=4)
